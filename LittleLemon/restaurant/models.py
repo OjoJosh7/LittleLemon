7fc6041676a0d44068dad2a1ec88926424e150bf
unknown <ojojosh97@gmail.com>
2023-09-01 13:08:58 +0100

    My seventh commit

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,18 +1,22 @@
 from django.db import models
-
+from datetime import datetime
+from decimal import Decimal
 # Create your models here.
-class Menu(models.Model):
+class Booking(models.Model):
     ID = models.IntegerField(primary_key=True,)
     Name = models.CharField(max_length=255)
     No_of_guests = models.IntegerField()
-    Bookingdate = models.DateTimeField()
+    Bookingdate = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self) -> str:
+        return self.Name
 
 
-class Booking(models.Model):
+class Menu(models.Model):
     ID = models.IntegerField(primary_key=True)
     Title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2 )
+    price = models.DecimalField(max_digits=10, decimal_places=2, default = 0.0 )
     Inventory = models.IntegerField()
 
     def __str__(self) -> str:
-        return self.Title
+        return f'{self.Title}:{self.price}'
